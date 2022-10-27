80afcef0b66408a941fe4d8e068052325d69ae52
nellybarasa <99465502+the1nomad@users.noreply.github.com>
2022-10-27 15:39:47 +0300

    cypress-scripts

---
+++ cypress/e2e/spec2.cy.js
@@ -0,0 +1,7 @@
+describe('newspec', () => {
+  it('doess not do much', () => {
+    cy.visit('https://dcamuat.dtbafrica.com/AppBuilder/#/0RaoHome?ProductID=CURLY')
+    cy.contains('type')
+  
+  })
+})
